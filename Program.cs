29fc982dda75b12d94390284ebd617b35b89b596
Sandr_Lite <mr.wlas@mail.ru>
2024-10-23 16:45:21 +0300

    Добавил сервис LogService, для добавление логов в приложение.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
 ﻿using DeliveryServiceFiltersOrders;
+using DeliveryServiceFiltersOrders.Service;
 using DeliveryServiceFiltersOrders.Service.EntityFramework;
+
+string logFileOutput = @"Logs\log_output.log";
+
+// Инициализация логирования
+LogService.Initialize(logFileOutput);
+LogService.Log("Запуск программы.");
 
 using var context = new DeliveryContext();
 
@@ -8,6 +15,11 @@ await orderService.ImportOrdersFromCsvAsync(@"Resources/orders.csv");
 
 Console.WriteLine("Введите район для фильтрации:");
 var district = Console.ReadLine();
+if(string.IsNullOrEmpty(district))
+{
+    Console.WriteLine("Ошибка, введите район для фильтрации");
+    return;
+}
 
 Console.WriteLine("Введите начальное время (yyyy-MM-dd HH:mm:ss):");
 var fromTime = DateTime.Parse(Console.ReadLine());
@@ -17,6 +29,7 @@ var toTime = DateTime.Parse(Console.ReadLine());
 
 Console.WriteLine("Введите минимальное количество заказов в районе:");
 var minOrdersInDistrict = int.Parse(Console.ReadLine());
+
 
 var filteredOrders = await orderService.GetFilteredOrdersAsync(district.Trim(), fromTime, toTime, minOrdersInDistrict);
 
@@ -34,3 +47,4 @@ else
     Console.WriteLine("Заказы отсутствуют по данному фильтру.");
 }
 
+LogService.Log("Завершение работы программы.");
---
+++ Service/LogService.cs
@@ -0,0 +1,24 @@
+﻿namespace DeliveryServiceFiltersOrders.Service
+{
+    public static class LogService
+    {
+        private static string _logFilePath;
+
+        public static void Initialize(string logFilePath)
+        {
+            _logFilePath = logFilePath;
+        }
+
+        public static void Log(string message)
+        {
+            try
+            {
+                File.AppendAllText(_logFilePath, $"{DateTime.Now}: {message}{Environment.NewLine}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка записи в лог файл: " + ex.Message);
+            }
+        }
+    }
+}
