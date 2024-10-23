b77a0e48f7e8c456ae67d714e6e79dbaf7310510
Sandr_Lite <mr.wlas@mail.ru>
2024-10-23 17:57:26 +0300

    # Валидация и логирование - Добавил валидацию ввода данных. - Добавил логирование в некоторые участки кода.

--- Program.cs
+++ Program.cs
@@ -4,47 +4,78 @@ using DeliveryServiceFiltersOrders.Service.EntityFramework;
 
 string logFileOutput = @"Logs\log_output.log";
 
-// Инициализация логирования
-LogService.Initialize(logFileOutput);
-LogService.Log("Запуск программы.");
-
-using var context = new DeliveryContext();
-
-var orderService = new OrderRepository(context);
-await orderService.ImportOrdersFromCsvAsync(@"Resources/orders.csv");
-
-Console.WriteLine("Введите район для фильтрации:");
-var district = Console.ReadLine();
-if(string.IsNullOrEmpty(district))
+try
 {
-    Console.WriteLine("Ошибка, введите район для фильтрации");
-    return;
-}
+    // Инициализация логирования
+    LogService.Initialize(logFileOutput);
+    LogService.Log("Запуск программы.");
 
-Console.WriteLine("Введите начальное время (yyyy-MM-dd HH:mm:ss):");
-var fromTime = DateTime.Parse(Console.ReadLine());
+    using var context = new DeliveryContext();
 
-Console.WriteLine("Введите конечное время (yyyy-MM-dd HH:mm:ss):");
-var toTime = DateTime.Parse(Console.ReadLine());
+    var orderService = new OrderRepository(context);
+    await orderService.ImportOrdersFromCsvAsync(@"Resources/orders.csv");
 
-Console.WriteLine("Введите минимальное количество заказов в районе:");
-var minOrdersInDistrict = int.Parse(Console.ReadLine());
-
-
-var filteredOrders = await orderService.GetFilteredOrdersAsync(district.Trim(), fromTime, toTime, minOrdersInDistrict);
-
-Console.WriteLine("Отфильтрованные заказы:");
-
-if (filteredOrders.Any())
-{ 
-    foreach (var order in filteredOrders)
+    Console.WriteLine("Введите район для фильтрации:");
+    var district = Console.ReadLine();
+    if (string.IsNullOrEmpty(district))
     {
-        Console.WriteLine(order.ToString());
+        message("Ошибка, не введен район.");
+        return;
+    }
+
+    Console.WriteLine("Введите начальное время (yyyy-MM-dd HH:mm:ss):");
+    DateTime fromTime;
+    if (!DateTime.TryParse(Console.ReadLine(), out fromTime))
+    {
+        message("Ошибка, неверный формат даты. Валидный формат: yyyy-MM-dd HH:mm:ss");
+        return;
+    }
+
+
+    Console.WriteLine("Введите конечное время (yyyy-MM-dd HH:mm:ss):");
+    DateTime toTime;
+    if (!DateTime.TryParse(Console.ReadLine(), out toTime))
+    {
+        message("Ошибка, неверный формат даты. Валидный формат: yyyy-MM-dd HH:mm:ss");
+        return;
+    }
+
+    Console.WriteLine("Введите минимальное количество заказов в районе:");
+    var minOrdersInDistrict = int.Parse(Console.ReadLine());
+
+
+    var filteredOrders = await orderService.GetFilteredOrdersAsync(district.Trim(), fromTime, toTime, minOrdersInDistrict);
+
+    Console.WriteLine("Отфильтрованные заказы:");
+
+    if (filteredOrders.Any())
+    {
+        LogService.Log($"Найдены следующие заказы по фильтру: Район - {district}, Начальное время - {fromTime}, Конечное время - {toTime}");
+
+        foreach (var order in filteredOrders)
+        {
+            LogService.Log(order.ToString());
+            Console.WriteLine(order.ToString());
+        }
+    }
+    else
+    {
+        LogService.Log($"Заказы отсутствуют по данному фильтру. Район: {district}, Начальное время: {fromTime}, Конечное время: {toTime}");
+        Console.WriteLine("Заказы отсутствуют по данному фильтру.");
     }
 }
-else
+catch (Exception ex)
 {
-    Console.WriteLine("Заказы отсутствуют по данному фильтру.");
+    message($"Ошибка: {ex.Message}");
+}
+finally
+{
+    LogService.Log("Завершение работы программы.");
 }
 
-LogService.Log("Завершение работы программы.");
+void message(string msg)
+{
+    LogService.Log(msg);
+    Console.WriteLine(msg);
+    Console.ReadKey(true);
+}
